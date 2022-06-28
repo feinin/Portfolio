078e542c296f5ba287ab05467499b14e86573dee
fenin <kongkenan@gmail.com>
2022-06-28 13:19:54 -0700

    etc

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // console.log('Kenan Website')
 
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('#navbar');
-    navbar.classList.toggle("sticky", window.scrollY > 0);
-})
+// still working  on this sticky nav bar
+// window.addEventListener('scroll', function() {
+//     const navbar = document.querySelector('#navbar');
+//     navbar.classList.toggle("sticky", window.scrollY > 0);
+// })
+
+// my submit form button
+function mySubmit() {
+    alert("Thanks For Submitting King/Queen");
+}
+// why you click on the button
+function why() {
+    alert("Why Did You Click ?!!!");
+}
+// good bye alert
+function myGood() {
+    const x = document.getElementById("myInput").value;
+    document.getElementById("demo").innerHTML = x;
+}
